7a360f0f88a1d27e6dfa4e65d04fa432e00ca174
Quvonch299 <muminovquvonchbek85@gmail.com>
2025-10-30 15:48:06 +0500

    Restore footer and map route; remove mobile stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Route, Routes } from 'react-router'
 import All from './home/All'
+import Map from './pages/Map'
+import Footer from './home/Footer'
+
 export default function App() {
   return (
     <div className='bg h-[100vh]'>
       <div className="max-w-7xl m-auto pt-9">
-
-        <All />
-       
+        <Routes>
+          <Route path='/' element={<All />} />
+          <Route path='/map' element={<Map />} />
+        </Routes>
       </div>
+
+      {/* Footer stays outside main container so it's visible on all pages */}
+      <Footer />
     </div>
   )
 }
